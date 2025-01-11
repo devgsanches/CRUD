bab28e52dada6235a4a256d70587f5e9ca7c8000
Guilherme Sanches <guilhermesanches.pessoal@gmail.com>
2025-01-11 20:16:08 -0300

    logics changes

--- app.js
+++ app.js
@@ -7,29 +7,42 @@ const { v4: uuidv4 } = require('uuid');
 
 const users = []
 
-// const myFirstMiddleware = (request, response, next) => {
-//     console.log('Fui chamado');
-
-// } // parou minha requisição, para que o fluxo ocorra normalmente..:
-
 // MIDDLEWARE
-const myFirstMiddleware = (request, response, next) => {
-    console.log('Fui chamado.'); // sempre executa primeiro
+// const myFirstMiddleware = (request, response, next) => {
+//     console.log('Fui chamado.'); // sempre executa primeiro
 
-    next() // continua o fluxo da aplicação, no caso, executar o endpoint que foi requisitado
+//     next() // continua o fluxo da aplicação, no caso, executar o endpoint que foi requisitado
 
-    console.log('Fluxo da aplicação finalizado!'); // depois que o fluxo da aplicação finaliza, ele volta ao middleware e continua a execução após o next
+//     console.log('Fluxo da aplicação finalizado!'); // depois que o fluxo da aplicação finaliza, ele volta ao middleware e continua a execução após o next
+
+// }
+
+const checkUserId = (req, res, next) => {
+    // (( verificação )) se o id é válido 
+    const { id } = req.params // capturando ID específico passado pela URL -> Route params
+    const index = users.findIndex((user) => {
+        return user.id === id
+    })
+
+
+    if (index < 0) {
+        return res.status(404).json({ error: "User not found" })
+    }
+
+    req.indexUser = index;
+    req.idUser = id; // PRIMEIRO
+
+    next() // CONTINUA FLUXO -> VAI PRA ROTA QUE FOI CHAMADA
+
+    console.log('Finalizei o fluxo.'); // ACABANDO DE EXECUTAR A ROTA CHAMADA, VOLTA E EXIBE A MSG NO CONSOLE
 
 }
 
-app.use(myFirstMiddleware)
 
 
 // Rotas da aplicação - endpoints - (envio/edit de dados)
 
 app.get('/users', (req, res) => {
-    console.log('next() - A rota foi chamada.');
-
     return res.json(users)
     // users por definição já é um array, então não precisamos indicar nada ao JSON, pois o mesmo aceita {} and [].
 })
@@ -42,41 +55,28 @@ app.post('/users', (req, res) => {
     return res.status(201).json(user)
 })
 
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params // capturando ID específico passado pela URL -> Route params
+app.put('/users/:id', checkUserId, (req, res) => {
+
     const { name, login, pass } = req.body
+    const { indexUser, idUser } = req
 
-    const updateUser = { id, name, login, pass }
+    const updateUser = { id: idUser, name, login, pass }
 
-    const index = users.findIndex((user) => {
-        return user.id === id
-    }
-    )
+
     // retorna o usuário de ID igual ao que passei pelas Route params, retornando, findIndex pega a posição desse usuário perante ao array
 
-    users[index] = updateUser
+    users[indexUser] = updateUser
     // atualiza o user capturado pelo ID, pelo mesmo user, porém atualizado. Essas atualizações são enviadas pelo Body
 
-    if (index < 0) {
-        res.status(404).json({ error: "User not found" })
-    }
 
     return res.json(updateUser)
 })
 
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params
+app.delete('/users/:id', checkUserId, (req, res) => {
+    const { indexUser } = req
 
-    const index = users.findIndex((e) => {
-        return e.id === id
-    })
-
-    if (index < 0) {
-        return res.status(404).json({ error: "User not found" })
-    }
-
-
-    users.splice(index, 1)
+    console.log(indexUser)
+    users.splice(indexUser, 1)
     return res.status(204).json()
 })
 
